0230e8df9df597b021b5b54f0008ebe1a3894e73
Madhu <madhusudhananit@gmail.com>
2016-05-14 06:52:50 +0530

    Corrected effects functions
    committed

--- core/effects.ts
+++ core/effects.ts
@@ -9,25 +9,25 @@ export class Effects{
     }
     
     grayScale(percent: number){
-         var d = this.pixels,  rp = 0.2126, gp = 0.7152, bp = 0.0722;            
-         for (var i = 0; i < d.length; i+=4) {
+         var d = this.pixels;  
+             var gamma = (percent / 100);
+            for (var i = 0; i < d.length; i+=4) {
                 var r = d[i];
                 var g = d[i + 1];
                 var b = d[i + 2];
-                var v = rp * r + gp * g + bp * b;
-                v = this.clampValue(v + (v * (percent/100)));
-                d[i] = d[i + 1] = d[i + 2] = v;
-         }
-         return this.pixels;
+                var v = 0.299 * r + 0.587 * g + 0.114 * b;                             
+                d[i] = d[i + 1] = d[i + 2] =  this.clampValue(v + (v * gamma));
+            }
+            return this.pixels;
     }
     
-    brighten(percent: number) {
-        var d = this.pixels; percent = 255 * (percent / 100);
+    brighten(amount: number /*amount - ranges from -1 to 1*/) {
+        var d = this.pixels; 
         for (var i = 0; i < d.length; i += 4) {
-            d[i] = this.clampValue(d[i] + percent);
-            d[i + 1] = this.clampValue(d[i + 1] + percent);
-            d[i + 2] = this.clampValue(d[i + 2] + percent);
-        }
+                d[i] = this.clampValue(d[i] * amount);
+                d[i + 1] = this.clampValue(d[i + 1] * amount);
+                d[i + 2] = this.clampValue(d[i + 2] * amount);
+            }
         return this.pixels;
     }
     invert() {
@@ -39,8 +39,28 @@ export class Effects{
         }
         return this.pixels;
     }
-    sepia(percent: number){                
-        
-        return this.pixels;
+    sepia(percent: number) {
+            var d = this.pixels, r, g, b, rp, gp, bp, 
+            pt = function (p) { return p - (p * (percent | 0 / 100)); };
+            
+            for (var i = 0; i < d.length; i += 4) {
+                r = d[i];
+                g = d[i + 1];
+                b = d[i + 2];
+                rp = (r * .393) + (g * .769) + (b * .189);
+                gp = (r * .349) + (g * .686) + (b * .168);
+                bp = (r * .272) + (g * .534) + (b * .131);                
+                d[i] = pt(rp); d[i + 1] = pt(gp); d[i + 2] = pt(bp);
+            }
+            return this.pixels;
     }
+    contrast(contrast: number /* ranges from 0-255*/) {
+            var d = this.pixels; var f = (259 * (contrast + 255)) / (255 * (259 - contrast))
+            for (var i = 0; i < d.length; i += 4) {
+                d[i] = this.clampValue(f * (d[i] - 128) + 128);
+                d[i + 1] = this.clampValue(f * (d[i + 1] - 128) + 128);
+                d[i + 2] = this.clampValue(f * (d[i + 2] - 128) + 128);
+            }
+            return this.pixels;
+        }
 }
